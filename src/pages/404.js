d4b48c5becde4d7cad1ece0390685a30fb99c346
Saumya Jain <samjain0913@gmail.com>
2021-08-08 19:00:47 -0400

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,12 +5,8 @@ import Layout from '../components/layout'
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist.... Wanna head back <a href="https://saumyajain.org/">Home</a> ?</p>
+    <p>This page does not exist.... Head back <a href="https://saumyajain.org/">Home</a> ?</p>
     <hr></hr>
-    
-    <h2>Don't be sad, it's CATURDAY!</h2>
-    <p>Here is a <strong>truly</strong> random cat for you :)</p> 
-    <img src="https://theoldreader.com/kittens/600/400"></img>
   </Layout>
 )
 
